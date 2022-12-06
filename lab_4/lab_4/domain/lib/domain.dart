a884958126da36347bb6d1f709a218721b3506c0
Cătălin Pleșu <catalinhimself@gmail.com>
2022-12-06 20:40:06 +0200

    pam lab 4 finished

--- lab_4/lab_4/domain/lib/domain.dart
+++ lab_4/lab_4/domain/lib/domain.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library domain;
-
-export 'src/domain_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+int calculate() {
+  return 6 * 7;
+}
--- lab_4/lab_4/domain/test/domain_test.dart
+++ lab_4/lab_4/domain/test/domain_test.dart
@@ -2,15 +2,7 @@ import 'package:domain/domain.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+  test('calculate', () {
+    expect(calculate(), 42);
   });
 }
